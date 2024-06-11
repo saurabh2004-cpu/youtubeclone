0aaff72684c4e31dda87b8f9a0f38285983f1cb2
saurabh <saurabhbodakhe2004@gmail.com>
2024-06-11 16:32:03 +0530

    -

--- src/components/Channel/ChannelHome.jsx
+++ src/components/Channel/ChannelHome.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
-import axiosInstance from '../../axiosInstance.js';
+// import axiosInstance from '../../axiosInstance.js';
+import axiosInstance from "../../axiosInstance.js"
 
 function ChannelHome() {
 
