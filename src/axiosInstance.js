a5839ebda43e2d9917f8b9421901f31ec3a93fee
saurabh <saurabhbodakhe2004@gmail.com>
2024-06-12 15:24:28 +0530

    -

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,12 +1,13 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://backend-project-7.onrender.com/api/v1',
+  baseURL: 'https://backend-project-7.onrender.com/api/v1', // Correct base URL
+  withCredentials: true, // To send cookies with requests
 });
 
 axiosInstance.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('accessToken'); // or wherever you store the token
+    const token = localStorage.getItem('accessToken');
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
