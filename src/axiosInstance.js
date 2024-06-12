2bb9d1e94b83b2bd19060fda15cfee7dc75a1a73
saurabh <saurabhbodakhe2004@gmail.com>
2024-06-12 10:38:28 +0530

    -

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiUrl = import.meta.env.VITE_API_URL || 'https://backend-project-7.onrender.com/api/v1;
+const apiUrl = import.meta.env.VITE_API_URL || 'https://backend-project-7.onrender.com/api/v1';
 
 const axiosInstance = axios.create({
   baseURL: apiUrl,
