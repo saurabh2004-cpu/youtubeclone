dc67ae70ea6fef07dcdbf705c2a4a030dad7ac89
saurabh <saurabhbodakhe2004@gmail.com>
2024-06-12 16:05:07 +0530

    -

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -5,17 +5,17 @@ const axiosInstance = axios.create({
   withCredentials: true, // To send cookies with requests
 });
 
-// axiosInstance.interceptors.request.use(
-//   (config) => {
-//     const token = localStorage.getItem('accessToken');
-//     if (token) {
-//       config.headers.Authorization = `Bearer ${token}`;
-//     }
-//     return config;
-//   },
-//   (error) => {
-//     return Promise.reject(error);
-//   }
-// );
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('accessToken');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
