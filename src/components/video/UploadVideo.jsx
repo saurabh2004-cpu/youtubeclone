9ea5518f09baf42f09a21d08e24c10c4aedd651d
saurabh <saurabhbodakhe2004@gmail.com>
2024-06-14 14:33:03 +0530

    -

--- src/components/video/UploadVideo.jsx
+++ src/components/video/UploadVideo.jsx
@@ -57,22 +57,22 @@ function UploadVideo() {
         }
     };
 
-    if(uploadProgress>0){
-        return (
-            <div className="relative pt-1">
-                <div className="flex mb-2 items-center justify-between">
-                    <div>
-                        <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-blue-600 bg-blue-200">
-                            Uploading {uploadProgress}%
-                        </span>
-                    </div>
-                </div>
-                <div className="overflow-hidden h-2 mb-4 text-xs flex rounded bg-blue-200">
-                    <div style={{ width: `${uploadProgress}%` }} className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-blue-500"></div>
-                </div>
-            </div>
-        )
-    }
+    // if(uploadProgress>0){
+    //     return (
+    //         <div className="relative pt-1">
+    //             <div className="flex mb-2 items-center justify-between">
+    //                 <div>
+    //                     <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-blue-600 bg-blue-200">
+    //                         Uploading {uploadProgress}%
+    //                     </span>
+    //                 </div>
+    //             </div>
+    //             <div className="overflow-hidden h-2 mb-4 text-xs flex rounded bg-blue-200">
+    //                 <div style={{ width: `${uploadProgress}%` }} className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-blue-500"></div>
+    //             </div>
+    //         </div>
+    //     )
+    // }
     
 
 
@@ -199,6 +199,21 @@ function UploadVideo() {
                                 </div>
                             </form>
                         )}
+
+                        {uploadProgress>0 &&
+                            <div className="relative pt-1">
+                                <div className="flex mb-2 items-center justify-between">
+                                    <div>
+                                        <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-blue-600 bg-blue-200">
+                                            Uploading {uploadProgress}%
+                                        </span>
+                                    </div>
+                                </div>
+                                <div className="overflow-hidden h-2 mb-4 text-xs flex rounded bg-blue-200">
+                                    <div style={{ width: `${uploadProgress}%` }} className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-blue-500"></div>
+                                </div>
+                            </div>
+                        }
 
                     </div>
                 </div>
