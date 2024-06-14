0c0c84144f98221dff3f87475c230d37b6a5e10d
saurabh <saurabhbodakhe2004@gmail.com>
2024-06-14 11:38:30 +0530

    -

--- src/components/video/UploadVideo.jsx
+++ src/components/video/UploadVideo.jsx
@@ -56,6 +56,24 @@ function UploadVideo() {
         }
     };
 
+    if(uploadProgress>0){
+        return (
+            <div className="relative pt-1">
+                <div className="flex mb-2 items-center justify-between">
+                    <div>
+                        <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-blue-600 bg-blue-200">
+                            Uploading {uploadProgress}%
+                        </span>
+                    </div>
+                </div>
+                <div className="overflow-hidden h-2 mb-4 text-xs flex rounded bg-blue-200">
+                    <div style={{ width: `${uploadProgress}%` }} className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-blue-500"></div>
+                </div>
+            </div>
+        )
+    }
+
+
     return (
         <>
             <Header showCatagories={false} />
@@ -175,20 +193,7 @@ function UploadVideo() {
                                     <button type="submit" className="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 transition-all duration-200">
                                         Upload Video
                                     </button>
-                                    {uploadProgress > 0 && (
-                                        <div className="relative pt-1">
-                                            <div className="flex mb-2 items-center justify-between">
-                                                <div>
-                                                    <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-blue-600 bg-blue-200">
-                                                        Uploading {uploadProgress}%
-                                                    </span>
-                                                </div>
-                                            </div>
-                                            <div className="overflow-hidden h-2 mb-4 text-xs flex rounded bg-blue-200">
-                                                <div style={{ width: `${uploadProgress}%` }} className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-blue-500"></div>
-                                            </div>
-                                        </div>
-                                    )}
+                                    
                                 </div>
                             </form>
                         )}
