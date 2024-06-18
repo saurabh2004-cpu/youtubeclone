63e06bc99d9f5f6376efc8db7889f7d92342955c
saurabh <saurabhbodakhe2004@gmail.com>
2024-06-18 14:38:11 +0530

    -updatin roter

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,47 @@
-// App.jsx
-import React, { useState } from 'react';
-import { Outlet } from 'react-router-dom';
-import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import {
+  Register,
+  Home,
+  ChannelProfile,
+  UploadVideo,
+  GetVideo,
+  ManageVideo,
+  EditVideo,
+  GetTweet,
+  UpdatePlaylist,
+  GetChannel,
+  LikedVideo,
+  WatchHistory,
+  WatchLater,
+  CreatePlayList,
+  GetPlayList,
+  GetAllPlaylists,
+} from './components/index.js';
+import Login from './components/Login.jsx';
 
 function App() {
-  const [loading, setLoading] = useState(false);
-
-  return !loading ? (
-    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
-      <div className='w-full block'>
-        <main>
-          <Outlet />
-        </main>
-      </div>
-    </div>
-  ) : null;
+  return (
+    <Routes>
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/channel-profile" element={<ChannelProfile />} />
+      <Route path="/upload-video" element={<UploadVideo />} />
+      <Route path="/getVideo/:videoId" element={<GetVideo />} />
+      <Route path="/manageVideo" element={<ManageVideo />} />
+      <Route path="/editVideo/:videoId" element={<EditVideo />} />
+      <Route path="/get-tweets/:channelId" element={<GetTweet />} />
+      <Route path="/create-playlist" element={<CreatePlayList />} />
+      <Route path="/get-playlist/:playlistId" element={<GetPlayList />} />
+      <Route path="/get-all-playlists/:channelId" element={<GetAllPlaylists />} />
+      <Route path="/update-playlist/:channelId" element={<UpdatePlaylist />} />
+      <Route path="/get-channel/:channelId" element={<GetChannel />} />
+      <Route path="/liked-videos" element={<LikedVideo />} />
+      <Route path="/watch-history" element={<WatchHistory />} />
+      <Route path="/watch-later" element={<WatchLater />} />
+    </Routes>
+  );
 }
 
 export default App;
