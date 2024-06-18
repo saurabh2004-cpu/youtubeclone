fb851ee2294ccc0891ba4069276262c3f3d067ab
saurabh <saurabhbodakhe2004@gmail.com>
2024-06-18 16:00:02 +0530

    -

--- src/store/channelSlice.js
+++ src/store/channelSlice.js
@@ -1,7 +1,30 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {
-    channelData: null,
+const loadStateFromLocalStorage = () => {
+    try {
+        const serializedState = localStorage.getItem('authState');
+        if (serializedState === null) {
+            return undefined;
+        }
+        return JSON.parse(serializedState);
+    } catch (e) {
+        console.warn("Could not load state from localStorage", e);
+        return undefined;
+    }
+};
+
+const saveStateToLocalStorage = (state) => {
+    try {
+        const serializedState = JSON.stringify(state);
+        localStorage.setItem('authState', serializedState);
+    } catch (e) {
+        console.warn("Could not save state to localStorage", e);
+    }
+};
+
+
+const initialState = loadStateFromLocalStorage() || {
+    userData: null
 };
 
 const channelSlice = createSlice({
@@ -10,9 +33,11 @@ const channelSlice = createSlice({
     reducers: {
         setChannel: (state, action) => {
             state.channelData = action.payload;
+            saveStateToLocalStorage(state);
         },
         clearChannel: (state) => {
             state.channelData = null;
+            localStorage.removeItem('authState');
         },
     },
 });
