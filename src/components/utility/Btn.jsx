da3a9ae333a0bae48f65de9a01507bc3612233a9
saurabh <saurabhbodakhe2004@gmail.com>
2024-06-18 20:55:08 +0530

    -

--- src/components/utility/Btn.jsx
+++ src/components/utility/Btn.jsx
@@ -3,7 +3,7 @@ import './utility.css'; // Import the utility.css file
 
 function Button({ text, ...props }) {
   return (
-    <button className="Button" {...props}>
+    <button className="my-button" {...props}>
       {text}
     </button>
   );
