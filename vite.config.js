6fb00c3be776d7e10d933e99c786aedfbe8e6d4f
saurabh <saurabhbodakhe2004@gmail.com>
2024-06-19 09:35:18 +0530

    -

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://backend-project-7.onrender.com',  // Adjust this to match your backend server address and port
+        target: 'https://localhost:800',  // Adjust this to match your backend server address and port
         rewrite: (path) => path.replace(/^\/api/, ''),
         
       },
